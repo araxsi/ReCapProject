e661a733ba942a6fa3f4cfb1ff61c7f564008110
Alex Yılmaz <araxis93@gmail.com>
2021-02-20 13:54:02 +0300

    12 Gün Entegrasyonu

---
+++ Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,28 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class ProductValidator : AbstractValidator<Cars>
+    {
+        public ProductValidator()
+        {
+            RuleFor(p => p.Description).MaximumLength(2);
+            RuleFor(p => p.BrandId).NotEmpty();
+            RuleFor(p => p.ColorId).NotEmpty();
+            RuleFor(p => p.DailyPrice).GreaterThan(0);
+            RuleFor(p => p.DailyPrice).GreaterThanOrEqualTo(10).When(p => p.BrandId == 1);
+            //Özel hata Mesajı verilirken kullanılan yöntemdir.
+            RuleFor(p => p.Description).Must(StartWithA).WithMessage("Ürünler A ile başlamalıdır.");
+
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
+    }
+}
---
+++ Core/Utilities/Interceptors/MethodInterceptionBaseAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Castle.DynamicProxy;
+using System;
+
+namespace Core.Utilities.Interceptors
+{
+    //https://github.com/engindemirog/NetCoreBackend/blob/master/Core/Utilities/Interceptors adresinden alınmıştır.
+    //MethodInterceptionBaseAttribute
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public abstract class MethodInterceptionBaseAttribute : Attribute, IInterceptor
+    {
+        public int Priority { get; set; }
+
+        public virtual void Intercept(IInvocation invocation)
+        {
+
+        }
+    }
+}
